1cdcfdaf5e4b7285ccc23e4a679076739a0ec56f
Cesar Yuri e Silva Santos <91036277+cesaryuri@users.noreply.github.com>
2024-02-07 02:14:19 -0300

    API and Class with Strategy

---
+++ Objetivos_Strategy.js
@@ -0,0 +1,54 @@
+// Interface
+class InterStrategy {
+  executeObjective() {}
+}
+
+// Objetivos concretos
+class DerrotarPlayerAzulObjective extends InterStrategy {
+
+  executeObjective() {
+    return `Objetivo: Eliminar todas as tropas dos continentes do player azul`;
+  }
+}
+
+class ConquistarEuropaObjective extends InterStrategy {
+
+  executeObjective() {
+    return `Objetivo: Conquistar todo território europeu`;
+  }
+}
+
+// Contexto
+class Player {
+  constructor(objectiveStrategy) {
+    this.objectiveStrategy = objectiveStrategy;
+  }
+
+  // Executar o objetivo atual
+  executeCurrentObjective() {
+    return this.objectiveStrategy.executeObjective();
+  }
+
+  // Escolher objetivo
+  setObjectiveStrategy(objectiveStrategy) {
+    this.objectiveStrategy = objectiveStrategy;
+  }
+}
+
+/* const player1 = new Player(new ConquistarEuropaObjective());
+const objetivo1 = player1.executeCurrentObjective();
+
+console.log(objetivo1);
+
+player1.setObjectiveStrategy(new DerrotarPlayerAzulObjective());
+const objetivo2 = player1.executeCurrentObjective();
+
+console.log(objetivo2);
+*/
+
+module.exports = {
+  InterStrategy,
+  DerrotarPlayerAzulObjective,
+  ConquistarEuropaObjective,
+  Player,
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const { DerrotarPlayerAzulObjective, ConquistarEuropaObjective, Player } = require("./Objetivos_Strategy");
+
+app.use(express.json());
+
+// Criar instâncias dos jogadores
+const player1 = new Player(new ConquistarEuropaObjective());
+const player2 = new Player(new DerrotarPlayerAzulObjective());
+
+// Executar objetivos para obter os textos
+const objetivo1 = player1.executeCurrentObjective();
+const objetivo2 = player2.executeCurrentObjective();
+
+// Criar array com os dados dos jogadores
+const playersData = [
+  { name: "Player1", objective: objetivo1 },
+  { name: "Player2", objective: objetivo2 },
+];
+
+// Rota para obter todos os jogadores cadastrados
+app.get("/player_info/", (req, res) => {
+    res.status(200).json(playersData);
+   
+  });
+
+// Rota para obter informações de um jogador específico
+app.get("/player_info/:index", (req, res) => {
+  const index = parseInt(req.params.index);
+  
+  if (index >= 0 && index < playersData.length) {
+    res.status(200).json(playersData[index]);
+  } else {
+    res.status(404).send("Jogador não encontrado.");
+  }
+});
+
+app.listen(4000, () => {
+  console.log("API RODANDO!");
+});
